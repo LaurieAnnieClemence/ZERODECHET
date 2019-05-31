61821cea3fee9d11b0372b8de55643a84c878482
Laurie <Laurie.Annie.Clemence@gmail.com>
2019-05-31 04:37:46 +0200

    PAGE D'ACCUEIL: déchets

---
+++ app/controllers/wastes_controller.rb
@@ -0,0 +1,29 @@
+class WastesController < ApplicationController
+  def index
+    @wastes = Waste.all
+  end
+
+  def show
+    @waste = Waste.find(params[:id])
+  end
+
+  def new
+    @waste = Waste.new
+  end
+
+  def create
+    @waste = Waste.new(waste_params)
+
+    if @waste.save
+      redirect_back fallback_location: root_path, notice: "déchet ajoutée, merci à vous!"
+    else
+      flash[:alert] = @waste.errors_to_sentence
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  private
+  def waste_params
+    params.require(:waste).permit(:name, :category_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :categories, only: [:index, :new, :create]
+  resources :wastes, only: [:index, :show, :new, :create]
 
   root to: "categories#index"
 end
