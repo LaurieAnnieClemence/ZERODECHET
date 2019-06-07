c3293aeb8cae82892d5cbc2d97f5503c6e956c4b
Laurie <Laurie.Annie.Clemence@gmail.com>
2019-06-08 00:12:27 +0200

    PAGE D'ACCUEIL: solutions et tableaux

---
+++ app/controllers/solutions_controller.rb
@@ -0,0 +1,17 @@
+class SolutionsController < ApplicationController
+  def create
+	  @solution = Solution.new(solution_params)
+
+	  if @solution.save
+	    redirect_back fallback_location: root_path, notice: "solution ajoutée, merci à vous!"
+	  else
+	    flash[:alert] = @solution.errors_to_sentence
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  private
+	def solution_params
+    params.require(:solution).permit(:content, :waste_id)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :categories, only: [:index, :new, :create]
   resources :wastes, only: [:index, :show, :new, :create]
+  resources :solutions, only: [:create]
 
   root to: "categories#index"
 end
