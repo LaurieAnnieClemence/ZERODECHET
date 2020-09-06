c7ca4f858f305a18499ffa5e78bea0ebfb16c308
Laurie <Laurie.Annie.Clemence@gmail.com>
2020-09-06 09:28:50 +0200

    Blog: model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,26 @@
 class ArticlesController < ApplicationController
+
   def index
+    @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_back fallback_location: root_path, notice: "article publié, merci à vous!"
+    else
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:content, :author)
+  end
+
 end
